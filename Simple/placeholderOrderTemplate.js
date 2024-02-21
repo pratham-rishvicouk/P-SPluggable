97af897c1d96c0ef23f3591565e2a7cd282f683a
pratham-rishvicouk <160015639+pratham-rishvicouk@users.noreply.github.com>
2024-02-21 16:15:24 +0530

    added component
    added pluggable component

---
+++ Simple/placeholderOrderTemplate.js
@@ -0,0 +1,42 @@
+﻿'use strict';
+define(function (require) {
+  const template = require('text!./placeholderOrderTemplate.html');
+  require('css!./placeholderOrderTemplate.css');
+
+  const ngModule = angular.module('placeholderOrderTemplate', []);
+  ngModule.component('placeholderOrderTemplate', {
+    template,
+    require: {
+      control: '^',
+    },
+    controller: [
+      '$scope',
+      'controlService',
+      '$q',
+      '$element',
+      function ($scope, controlService, $q, $element) {
+        this.isMinimizeAllowed = controlService.isMinimizeAllowed();
+        const self = this;
+
+        this.selectedOrders = [];
+
+        this.windowTitle = 'OOB Placeholder Template';
+
+        this.selectedRow = null;
+
+        this.$onInit = () => {
+          self.selectedOrders = self.control.data.selectedOrders;
+        };
+
+        this.close = (data) => {
+          this.control.close(data);
+        };
+      },
+    ],
+  });
+
+  return {
+    exports: ngModule.name,
+    selector: 'placeholder-order-template',
+  };
+});
---
+++ ooplaceholderComponent.js
@@ -0,0 +1,52 @@
+﻿'use strict';
+define(function (require) {
+  const placeholderManager = require('core/placeholderManager');/* doubt */ /* How it will decide the place? */
+  const openorderWindow = require('../Simple/placeholderOrderTemplate.js');
+
+  const placeHolder = function ($scope, $element, controlService) {
+    this.getItems = () => {
+      const items = [
+        {
+          text: 'Print S & P', // Button name
+          key: 'placeholderOrderTemplate', // Button id (unique)
+          icon: 'fa fa-th-large', // Button icon
+          content: {
+            moduleName: 'placeholderOrderTemplate', // same as ngModule.name in placeholderOrderTemplate.js
+            controlName: 'placeholderOrderTemplate', // same as ngModule.name in placeholderOrderTemplate.js
+          },
+        },
+      ];
+
+      return items;
+    };
+
+    this.isEnabled = (itemKey) => {
+      return true;
+    };
+    /* will we be using below code? */
+    this.onClick = () => {
+      const control = controlService.createControl({
+        selector: openorderWindow.selector,
+        isPersistent: true,
+        data: {
+          selectedOrders: $scope.viewStats.get_selected_orders(),
+        },
+      });
+
+      control.load().then(() => {
+        control.show();
+      });
+
+      control.promise.then((data) => {
+        if (data?.search) {
+          $scope.$ctrl.control.$scope.$ctrl.search_value = data.search;
+          $scope.$ctrl.control.$scope.$ctrl.search_text();
+
+          $scope.$evalAsync();
+        }
+      });
+    };
+  };
+  /* what's this */
+  placeholderManager.register('OpenOrders_OrderControlButtons', placeHolder);
+});
